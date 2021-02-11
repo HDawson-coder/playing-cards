e2defea38f79b44b6b90e0cecbef04e2a54390a6
Hana Dawson <stubbornunicorn95@gmail.com>
2021-02-11 10:55:20 -0700

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/model/Card.java
@@ -0,0 +1,40 @@
+package edu.cnm.deepdive.model;
+
+public class Card implements Comparable<Card> {
+
+  private final Rank rank;
+  private final Suit suit;
+
+  public Card(Rank rank, Suit suit) { //constructor
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  public Rank getRank() { //getter
+    return rank;
+  }
+
+  public Suit getSuit() { //getter
+    return suit;
+  }
+
+  public String getSymbol() {
+    return String.format("%s%s", rank.symbol(), suit.symbol());
+  }
+
+  @Override //override notifies compiler that we are doing an override to 'keep us honest'
+  public String toString() {
+    //return String.format("%s %s", rank, suit);
+    return getSymbol();
+  }
+
+  @Override
+  public int compareTo(Card other) {
+    int comparison = suit.compareTo(other.suit);
+    if (comparison == 0) {
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
+  }
+  //%s means substitute, it's telling rank to replace first, and suit to replace second object
+}
---
+++ src/main/java/edu/cnm/deepdive/model/Deck.java
@@ -0,0 +1,48 @@
+package edu.cnm.deepdive.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Deck implements Dealable {
+
+  private List<Card> cards; //refers to <> below
+
+  //connects to constructor from card
+  public Deck() {
+    cards = new ArrayList<>();
+    for (Suit suit : Suit.values()) {
+      for (Rank rank : Rank.values()) {
+        Card card = new Card(rank, suit); //invocation
+        cards.add(card);
+      }
+    }
+  }
+
+  @Override
+  public void shuffle() {
+    Collections.shuffle(cards);
+  }
+
+  @Override
+  public void shuffle(Random rng) {
+    Collections.shuffle(cards, rng); //using the method that already exists to shuffle
+  }
+
+  @Override
+  public Dealable deal(int count) {
+    return null; //TODO Implement using private constructor.
+  }
+
+  @Override
+  public void sort() {
+    Collections.sort(cards);
+  }
+
+  @Override
+  public String toString() {
+    return cards.toString();
+  }
+
+}
